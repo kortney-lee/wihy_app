c09f99e516296a5195e899aa8071c307875afeea
kortney-lee <kortney_lee@hotmail.com>
2025-10-12 20:54:27 -0500

    Delete rss-polling-service directory

--- rss-polling-service/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import rssRoutes from './rss.js';
-
-const router = express.Router();
-
-// Integrate RSS routes
-router.use('/rss', rssRoutes);
-
-export default router;
