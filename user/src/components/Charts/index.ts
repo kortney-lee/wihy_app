3a35907e525bfefe6289a75889183df9b1a89187
Kortney <kortney_lee@hotmail.com>
2025-10-15 13:53:26 -0500

    Remove user folder - moved to separate repository at https://github.com/kortney-lee/wihy_user_ui

--- user/src/components/Charts/index.ts
+++
@@ -1,7 +0,0 @@
-// Chart Components Export Index
-export { default as WeightTrendChart } from './WeightTrendChart';
-export { default as BMIBodyFatChart } from './BMIBodyFatChart';
-export { default as BodyCompositionChart } from './BodyCompositionChart';
-export { default as NutritionMacroChart } from './NutritionMacroChart';
-export { default as SleepChart } from './SleepChart';
-export { default as ActivityChart } from './ActivityChart';
